7a666eb36182b72aad671f8a7a596be4133e25d6
jvfe <jvfecav@gmail.com>
2020-08-27 11:17:34 -0300

    feat: Add initial functionality
    Functions to parse the pandas columns and return the reformatted dict,
    as well as a function to perform the request itself.

--- reconciler/reconcile.py
+++ reconciler/reconcile.py
@@ -1,5 +1,7 @@
-import pandas as pd
+from collections import defaultdict
+import json
 import requests
+import pandas as pd
 
 test_df = pd.DataFrame(
     {
@@ -10,4 +12,34 @@ test_df = pd.DataFrame(
 
 
 def get_query_dict(df_column):
-    pass
+    """Convert a pandas DataFrame column to a query dictionary
+
+    The reconciliation API requires a json request formatted in a
+    very particular way. This function takes in a DataFrame column
+    and reformats it.
+
+    Args:
+        df_column: A pandas dataframe column.
+
+    Returns:
+        A dict with the column values reformatted.
+    """
+    values = df_column.unique()
+    reformatted = defaultdict(dict)
+
+    for idx, value in enumerate(values):
+
+        reformatted[f"q{idx}"] = {"query": value}
+
+    return reformatted
+
+
+def reconcile_column(df_column):
+    """Send reformatted dict for reconciliation"""
+
+    reformatted = get_query_dict(df_column)
+    reconcilable = {"queries": json.dumps(reformatted)}
+
+    response = requests.post("https://wikidata.reconci.link/en/api", data=reconcilable)
+
+    return response.json()
