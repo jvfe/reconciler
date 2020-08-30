ac343fec8232296a3a91db0c081ade1485cee271
jvfe <jvfecav@gmail.com>
2020-08-30 12:07:27 -0300

    Deployed 64c374f with MkDocs version: 1.1.2

--- docs/authors.md
+++
@@ -1,7 +0,0 @@
-## Development Lead
-
-* João Vitor F. Cavalcante (jvfe)
-
-## Contributors
-
-None yet. Why not be the first?
--- reconciler/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Top-level package for reconciler."""
-
-name = "reconciler"
-__author__ = """João Vitor F. Cavalcante"""
-__email__ = "jvfe@ufrn.edu.br"
-__version__ = "0.1.0"
-
-from reconciler.reconcile import reconcile
