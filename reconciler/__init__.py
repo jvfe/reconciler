52f3ba7ea91926a984662e04c4c0701b6537932e
jvfe <jvfecav@gmail.com>
2020-09-04 07:54:53 -0300

    Bump version 0.1.0 -> 0.1.1

--- reconciler/__init__.py
+++ reconciler/__init__.py
@@ -3,6 +3,6 @@
 name = "reconciler"
 __author__ = """João Vitor F. Cavalcante"""
 __email__ = "jvfe@ufrn.edu.br"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from reconciler.reconcile import reconcile
