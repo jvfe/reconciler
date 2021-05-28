ea3035afaf8840594109f402a86424e0ef61298a
Douglas Mennella <douglas.mennella@gmail.com>
2021-05-28 18:09:19 +0900

    Trap both IndexError and KeyError

--- reconciler/webutils.py
+++ reconciler/webutils.py
@@ -121,7 +121,7 @@ def parse_raw_results(input_keys, response):
                 current_df.drop(["features"], axis=1, inplace=True)
                 current_df["type_id"] = [item[0]["id"] for item in current_df["type"]]
                 current_df["type"] = [item[0]["name"] for item in current_df["type"]]
-            except IndexError:
+            except (IndexError, KeyError):
                 pass
 
         current_df["input_value"] = input_keys[idx]
